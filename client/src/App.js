a7280c622a8986ecd52eb99d8960372479a7658c
brunokoppel <bruno.koppel@gmail.com>
2020-07-17 20:28:19 -0400

    rolling out first demo

--- client/src/App.js
+++ client/src/App.js
@@ -1,60 +1,35 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
+import {Container, Col, Row} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import Brunokoppel1 from './images/brunokoppel1.JPG';
-import Brunokoppel2 from './images/brunokoppel2.jpg';
-//import BG from './images/background.png';
-
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <div className="Header">
-        <table>
-          <tr>
-            <th>
-              <img src={Brunokoppel2} id="Avatar" alt="logo" />
-            </th>
-            <th>
-            <div id="text-container">
-              <h1 id="Bruno-Name">
-                Bruno Köppel
-              </h1>
-              <h2 id="Bruno-Description">
-                <p id="Bruno-Description-individual">Programmer</p>
-                <p id="Bruno-Description-individual">Musician</p>
-                <p id="Bruno-Description-individual">Tinkerer</p>
-              </h2>
+    <div>
+      
+      <Container style={{padding: "5vmin"}} fluid>
+        <Row style={{ display: "flex", flexDirection: "row"}}>
+          <Col sm={2} className="bg-dark" style={{margin: "0.5vmin", padding: "2vmin"}}>
+            <div style={{fontFamily: "monaco", color: "#FFFFFF" ,textAlign: "justify", textJustify: "inter-character"}} >
+              "As a native Cuban, I was lead to believe that my performance and freedom were tied to what the Cuban government thought I would be best at. But once I left the island, I was blessed with the opportunity to pursue the freedom and the dreams that appeal to my personal desires." 
             </div>
-            </th>
-          </tr>
-        </table>
-        
-        
-				
-        <div className="SideBar">
-          <table id="SideBarButtons">
-            <tr>
-             <Button id="SideBarButtonsSingle" size="md" variant="dark" href="https://github.com/BrunoKoppel">Github</Button>
-            </tr>
-            <tr>
-              <Button id="SideBarButtonsSingle" size="md" variant="dark" href="https://www.linkedin.com/in/bruno-koppel">LinkedIn</Button>
-            </tr>
-            <tr>
-            <Button id="SideBarButtonsSingle" size="md" variant="dark" href="https://open.spotify.com/artist/5XK2KlEZ95yiEOqE8BcbgE?si=O6LJDkVWTc-bZ6QWzuuuGA">Spotify</Button>
-            </tr>
-          </table>
-        </div>
-
-      </div>
-
+          </Col>
+          <Col sm={9} className="bg-light" style={{margin: "0.5vmin", padding: "2vmin"}}>
+            <p>
+              <div style={{fontFamily: "monaco", fontSize: "20px",textAlign: "justify", textJustify: "inter-character"}}>
+                Hello traveller, i'm just doing some work here on my page. i'm working on something very special for my platform, something that I've been planning for a while. It may take me a while to get out in time for you to enjoy but for now you can still access my regular links right next to my name. Enjoy my page and conctact me for any work!
+              </div>
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    
+        <footer>
+          <p>Author: Bruno Köppel @2020</p>
+        </footer>
       <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
       <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
       <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-
     </div>
-    
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Image, Navbar, Nav, Button } from 'react-bootstrap'
 import './index.css';
 import App from './App';
 import blog from './subpages/blog'
 import * as serviceWorker from './serviceWorker';
+import Brunokoppel2 from './images/brunokoppel2.jpg';
+import BG from './images/background.jpg';
+import GH from './images/GH.png'
+import LI from './images/LinkedInBnW.png'
+import SP from './images/spotifyBnW.png'
+import IG from './images/IG-BnW.png'
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
         <Route exact path='/blog' component={blog} />
     </Router>
-    <App/>
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="/">
+        <Button size="sm" variant="light">
+          <h3 style={{padding: "0.4vmin",margin: "0.2vmin" , color: "#000000", fontSize: "5vmin", fontFamily: "Monaco"}}>Bruno Köppel</h3>
+        </Button>
+      </Navbar.Brand>
+      <Nav className="mr-auto" style={{display: "flex", flexDirection: "row"}}>
+        
+        <Nav.Link style={{margin: "0.2vmin" }} href="https://github.com/BrunoKoppel">
+          <Button size="sm" variant="light">
+            <Image src={GH} style={{width: "5vmin"}} alt="logo" rounded fluid/>
+          </Button>
+        </Nav.Link>
+
+        <Nav.Link style={{margin: "0.2vmin" }} href="https://www.linkedin.com/in/bruno-koppel">
+          <Button size="sm" variant="light">
+            <Image src={LI} style={{width: "5vmin"}} alt="logo" rounded fluid/>
+          </Button>
+        </Nav.Link>
+
+        <Nav.Link style={{margin: "0.2vmin" }} href="https://open.spotify.com/artist/5XK2KlEZ95yiEOqE8BcbgE?si=O6LJDkVWTc-bZ6QWzuuuGA">
+          <Button size="sm" variant="light">
+            <Image src={SP} style={{width: "5vmin"}} alt="logo" rounded fluid/>
+          </Button>
+        </Nav.Link>
+
+        <Nav.Link style={{margin: "0.2vmin" }} href="https://www.instagram.com/bruno_koppel/">
+          <Button size="sm" variant="light">
+            <Image src={IG} style={{width: "5vmin"}} alt="logo" rounded fluid/>
+          </Button>
+        </Nav.Link>
+
+        <Nav.Link style={{margin: "0.2vmin", right: "2vmin" , position: "absolute"}} aria-controls="basic-navbar-nav">
+          <Button size="sm" variant="light">
+            <Image src={Brunokoppel2} style={{width: "5vmin"}} alt="logo" rounded fluid/>
+            
+          </Button>
+        </Nav.Link>
+        
+      </Nav>
+    </Navbar>
+    <App style={{backgroundImage: `url(${BG}))`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center' }}>
+      
+    </App>
   </React.StrictMode>,
   document.getElementById('root')
 );
